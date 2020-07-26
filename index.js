b0d0043fe4cc82b03393c5cce9654335e0be6695
EstebanDroguett <estebandroguetthuerta@gmail.com>
2020-07-25 20:19:50 -0400

    Upload Backend

--- index.js
+++ index.js
@@ -13,8 +13,16 @@ const cors = require('cors');
 var app = express();
 
 //CORS
-app.use(cors());
+//app.use(cors());
 //app.options('*', cors());
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 //Body-parser
 // parse application/x-www-form-urlencoded
